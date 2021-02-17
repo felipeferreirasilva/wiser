3eedad50359efb30b8ec870c19b52a9851ea02f1
Felipe Silva <felipeferreirasilva@gmail.com>
2021-02-17 15:45:15 -0300

    install sass and styledComponents

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,7 @@
-function MyApp({ Component, pageProps }) {
-  return (
-    <>
-      <Component {...pageProps} />
-    </>
-  )
+import "../public/styles/global.scss";
+
+function App({ Component, pageProps }) {
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default App;
